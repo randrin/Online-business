3ab5d3b21e681c113282a67315fb832112a4a63a
Randrin <nzeukangrandrin@gmail.com>
2018-08-03 22:04:00 +0200

    Set up Projects

---
+++ businessbackend/src/main/java/com/eninse/businessbackend/App.java
@@ -0,0 +1,13 @@
+package com.eninse.businessbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
