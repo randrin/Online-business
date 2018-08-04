db60947be9886e8c9e2caa659763833a04decb52
Randrin <nzeukangrandrin@gmail.com>
2018-08-04 09:31:49 +0200

    Template Front End. Add bootstrap/jquery/css and configuration with spring

--- onlinebusiness/src/main/java/com/eninse/onlinebusiness/controller/PageController.java
+++ onlinebusiness/src/main/java/com/eninse/onlinebusiness/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.eninse.onlinebusiness.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -11,6 +13,26 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Online Business project");
+		return mv;
+	}
+	
+	@RequestMapping(value="/test")
+	public ModelAndView test(@RequestParam(value="test", required=false)String test) {
+		ModelAndView mv = new ModelAndView("page");
+		if (test == null) {
+			test = "Hello user";
+		}
+		mv.addObject("greeting", test);
+		return mv;
+	}
+	
+	@RequestMapping(value="/test/{test}")
+	public ModelAndView testPath(@PathVariable("test")String test) {
+		ModelAndView mv = new ModelAndView("page");
+		if (test == null) {
+			test = "Hello user";
+		}
+		mv.addObject("greeting", test);
 		return mv;
 	}
 }
